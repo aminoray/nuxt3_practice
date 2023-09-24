ffbad62cc9cc167b89ad184b32f40c998381b696
ask-nugey <mischief.moi@gmail.com>
2023-09-24 16:28:38 +0900

    lottieページとコンポーネント作成

---
+++ components/organisms/Card/LottieCard/LottieCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import '~/components/organisms/Card/LottieCard/lottie-card.scss';
+
+type Props = {
+  title: string;
+  image: string;
+  text?: string;
+  date?: string;
+};
+withDefaults(defineProps<Props>(), {
+  title: '',
+  image: '',
+  text: '',
+  date: '',
+});
+</script>
+<template>
+  <div class="lottie-card">
+    <slot />
+    <div class="image">
+      <img v-if="image" :src="assetImage(image)" alt="" />
+    </div>
+    <div class="content">
+      <p class="title">{{ title }}</p>
+      <div v-if="text || date" class="info">
+        <p v-if="text" class="text">{{ text }}</p>
+        <p v-if="date" class="data">{{ date }}</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/lottie.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import '~/assets/scss/pages/lottie-container.scss';
+import LottieCard from '~/components/organisms/Card/LottieCard/LottieCard.vue';
+</script>
+<template>
+  <div class="lottie-container">
+    <div class="top-content">
+      <h1 class="big-title">Lottie</h1>
+    </div>
+
+    <div class="content">
+      <LottieCard
+        title="タイトル"
+        image="background-images/bg-002.png"
+        text="テキスト"
+        date="・ Apr 17-22"
+      />
+    </div>
+  </div>
+</template>
